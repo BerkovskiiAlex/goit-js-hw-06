3dc89f6d9b6303f03c2f7da33bd365e16f0c7f12
BerkovskiiAlex <berkovskii845@gmail.com>
2023-06-23 20:16:02 +0300

    create task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
+/** @format */
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulIngredients = document.querySelector("#ingredients");
+
+const mappedIngridients = ingredients.map((ingridient) => {
+  const ingridientsListItem = document.createElement("li");
+
+  ingridientsListItem.textContent = ingridient;
+  ingridientsListItem.classList.add("item");
+  return ingridientsListItem;
+});
+
+ulIngredients.append(...mappedIngridients);
+
+console.log(ulIngredients);
