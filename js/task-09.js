b4f9fd3263524f90842f699b7631ecd37133dce0
BerkovskiiAlex <berkovskii845@gmail.com>
2023-06-26 18:13:56 +0300

    create task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+/** @format */
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const body = document.querySelector("body");
+const span = body.querySelector("span");
+body.addEventListener("click", changeColor);
+const onButton = body.querySelector("span");
+
+function changeColor(event) {
+  const changedColor = getRandomHexColor();
+  span.textContent = changedColor;
+  body.style.backgroundColor = changedColor;
 }
